39e2996fac9aa26a725ba5c8c5a925381d9c4e4c
Wallysson Nunes <wally.nm@gmail.com>
2015-06-16 14:23:44 -0300

    Atualizado eventos para tap

--- js/sidebar.js
+++ js/sidebar.js
@@ -8,11 +8,28 @@
  !(function () {
    'use strict';
 
-   $('.ni-overlay').on('touchstart', function() {
+   $('.ni-container').on('swipeleft swiperight', function(e) {
+     alert('aff')
+     var $el = $(e.target);
+
+     // valida se o swipe esta sendo executado em um slider
+     if( $el.parents('.ni-slider').length === 0){
+
+       var stateMenu = !$('#menu').prop( "checked");
+       console.warn(stateMenu);
+       $('#menu').prop("checked", stateMenu);
+       console.warn(e);
+     }
+
+
+     //$('.ni-menu-checkbox:checked').prop( "checked", false );
+   });
+
+   $('.ni-overlay').on('tap', function() {
      $('.ni-menu-checkbox:checked').prop( "checked", false );
    });
 
-   $('.ni-menu').on('touchstart', function(e) {
+   $('.ni-menu').on('tap', function(e) {
      e.stopPropagation();
      var indx = $(e.target).is('label') ? -2 : -1;
      if( !$('.ni-menu-checkbox:checked + .ni-menu').eq(indx).is(this)){
@@ -20,7 +37,7 @@
      }
    });
 
-   $('.ni-menu a').on('touchstart', function(){
+   $('.ni-menu a').on('tap', function() {
      var clickedMenu = $(this).parents('.ni-menu');
      var currentMenu = $('.ni-menu-checkbox:checked + .ni-menu:last');
      if(!clickedMenu.is(currentMenu)){

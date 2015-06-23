1c5f59ba8bd8a51adbe09e06bab60fd85b4aca73
Wallysson Nunes <wally.nm@gmail.com>
2015-06-23 12:40:24 -0300

    Adicionado função responsavel por validar se o usuário chegou ao rodapé do sistema e caso o tenha feito

--- dist/js/ratchet.js
+++ dist/js/ratchet.js
@@ -449,12 +449,24 @@
     var elemTop = $elem.offset().top;
     var elemBottom = elemTop + $elem.height() - barHeight;
 
+
+
     return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
   };
 
   if ($('.ni-product-buy').length !== 0){
-    $('.content').scroll(function() {
+    var $content = $('.content');
+    var toolbarHeight = $('.ni-toolbar').height();
+    $content.css('padding-bottom', toolbarHeight);
+
+    $content.scroll(function() {
       var scrolled = isScrolledIntoView('.ni-product-buy');
+
+      // Caso o scroll chegue ao final da tela o metodo retorna true
+      if ($content[0].scrollHeight - $content.scrollTop() - toolbarHeight <= $content.innerHeight()) {
+        $('.ni-toolbar').addClass('ni-active');
+        return;
+      }
 
       if (scrolled && $('.ni-toolbar').hasClass('ni-active')) {
         $('.ni-toolbar').toggleClass('ni-active');

ee7bc8b2b0c62ee15f19f8c6c43ca56264f3cd34
Wallysson Nunes <wally.nm@gmail.com>
2015-08-28 13:39:36 -0300

    Otimizado plugin de counter dando suporte para valores inline, 2 tipos de visualização do plugin e agrupado a execução de alteração de valor do mesmo no plugin

--- js/counter.js
+++ js/counter.js
@@ -12,91 +12,114 @@
  * @Author: Luiz Gustavo, Wallysson Nunes
  * ======================================================================== */
 
-(function( $ ){
+(function($) {
   $.fn.counter = function(options) {
 
-  	var defaults = {
-			type : 'ni-counter',
-			min  : 1,
-			max  : 10,
-			customPlus: undefined,
-			beforeCustomPlus: undefined,
-			customMinus: undefined,
-			beforeCustomMinus: undefined,
-			maxLimitMsg: "O limite máximo foi atingido",
-			minLimitMsg: "O limite minino foi atingido",
+    var defaults = {
+      type: 'inline',
+      min: 1,
+      max: 10,
+      value: 0,
+      customPlus: undefined,
+      beforeCustomPlus: undefined,
+      customMinus: undefined,
+      beforeCustomMinus: undefined,
+      maxLimitMsg: "O limite máximo foi atingido",
+      minLimitMsg: "O limite minino foi atingido",
     };
 
-    var settings = $.extend( {}, defaults, options );
-
     // template do contador, com variação da classe que distingue os tipos de exibição.
-    var template = '<div class="'+settings.type+'"><div class="ni-more"><button>+</button></div><div class="ni-value" data-value="'+settings.min+'">'+settings.min+'</div><div class="ni-less"><button>-</button></div></div>';
+    var template = '<div><div class="ni-more"><button>+</button></div><div class="ni-value" data-value="' + settings.min + '">' + settings.min + '</div><div class="ni-less"><button>-</button></div></div>';
 
-    return this.each (function() {
-    	// Renderiza o template no elemento selecionado.
-    	var el = $(template);
+    var getBaseClass = function(type) {
+      return(type === 'inline') ? 'ni-counter' : 'ni-counter2';
+    }
+
+    var updateValue = function(num, $el) {
+      // Fechando o Tooltip
+      $el.tooltip('destroy');
+
+      // Valida se o valor do contador esta acima do permitido
+      if(num > settings.max) {
+        $el.tooltip({
+          title: settings.maxLimitMsg
+        }).tooltip('show');
+        return;
+      } else if(num < settings.min) {
+        $el.tooltip({
+          title: settings.minLimitMsg
+        }).tooltip('show');
+        return;
+      }
+
+      // Seta o valor no atributo
+      $el.data('value', num);
+      $el.find('.ni-value').text(num);
+    }
+
+    var settings = $.extend({}, defaults, options);
+
+    return this.each(function() {
       var self = this;
-      $(this).append(el);
+      var $el = $(this);
+
+      if($el.data('type'))
+        settings.type = $el.data('type');
+
+      if($el.data('value'))
+        settings.value = $el.data('value');
+
+      if($el.data('min')) {
+        settings.min = $el.data('min');
+
+        if(settings.value < settings.min) {
+          settings.value = settings.min;
+        }
+      }
+
+      if($el.data('max')) {
+        settings.max = $el.data('max');
+      }
+
+      if(typeof $el.data('value') === 'undefined') {
+        $el.data('value', settings.value);
+      }
+
+      // Renderiza o template no elemento selecionado.
+      var $tpl = $(template).addClass(getBaseClass(settings.type));
+
+      // Aplica o template
+      $el.html($tpl);
+
+      updateValue(settings.value, $el);
 
       /* Evento de clique para incrementar o valor. */
-      $(this).find('.ni-more').on('click', function() {
-      	// Executa a função beforeCustomPlus caso tenha sido configurada.
-      	if(typeof settings.beforeCustomPlus === 'function')
-      		beforeCustomPlus();
+      $el.find('.ni-more').on('click', function() {
+        // Executa a função beforeCustomPlus caso tenha sido configurada
+        if(typeof settings.beforeCustomPlus === 'function')
+          beforeCustomPlus();
 
-      	// Fechando o Tooltip
-      	$(el).tooltip('destroy');
+        updateValue($el.data('value') + 1, $el);
 
-      	// Elemento que exibe o valor do contador.
-      	var $val = $(this).parent().find('.ni-value');
-
-      	// Valor convertido para inteiro.
-      	var num = parseInt($val.text()) + 1;
-
-      	// Valida se o valor do contador esta acima do permitido.
-      	if(num > settings.max) {
-          $(el).tooltip({ title: settings.maxLimitMsg }).tooltip('show');
-      		return;
-      	}
-
-      	// Seta o valor no atributo.
-      	$val.text(num).attr("data-value", num);
-
-      	// Executa a função customPlus caso tenha sido configurada.
-      	if(typeof settings.customPlus === 'function')
-      		customPlus();
+        // Executa a função customPlus caso tenha sido configurada
+        if(typeof settings.customPlus === 'function')
+          customPlus();
       });
 
-      /* Evento de click para decrementar o valor. */
+      /* Evento de click para decrementar o valor */
       $(this).find('.ni-less').on('click', function() {
 
-      	// Executa a função beforeCustomMinus caso tenha sido configurada.
-      	if(typeof settings.beforeCustomMinus === 'function')
-      		beforeCustomMinus();
+        // Executa a função beforeCustomMinus caso tenha sido configurada
+        if(typeof settings.beforeCustomMinus === 'function')
+          beforeCustomMinus();
 
-      	// Destroi o Tooltip
-      	$(el).tooltip('destroy');
+        updateValue($el.data('value') - 1, $el);
 
-      	// Elemento que exibe o valor do contador.
-      	var $val = $(this).parent().find('.ni-value');
+        // Executa a função customMinus caso tenha sido configurada
+        if(typeof settings.customMinus === 'function')
+          customMinus();
 
-      	// Valor do elemento convertido para inteiro.
-      	var num = parseInt($val.text()) - 1;
-
-      	// Valida se o valor do contador esta abaixo do permitido.
-      	if(num < settings.min) {
-          $(el).tooltip({ title: settings.minLimitMsg }).tooltip('show');
-      		return;
-      	}
-
-      	// Seta o valor no atributo.
-      	$val.text(num).attr("data-value", num);
-
-      	// Executa a função customMinus caso tenha sido configurada.
-      	if(typeof settings.customMinus === 'function')
-      		customMinus();
       });
-
     });
   };
-})( jQuery );
+})(jQuery);

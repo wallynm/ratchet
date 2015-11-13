f5e456941487eaa73c86d1df86eb8c4fd25d5281
Wallysson Nunes <wally.nm@gmail.com>
2015-11-13 15:45:57 -0200

    Update dos arquivos source

--- dist/js/ratchet.js
+++ dist/js/ratchet.js
@@ -575,10 +575,8 @@
       if($el.data('value'))
         settings.value = $el.data('value');
 
-      if($el.data('name')){
-        alert($el.data('name'))
+      if($el.data('name'))
         settings.name = $el.data('name');
-      }
 
       if($el.data('min')) {
         settings.min = $el.data('min');

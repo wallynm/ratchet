67ef8b5148f116d22af857ef34ce613871b3f6c0
Wallysson Nunes <wally.nm@gmail.com>
2016-02-01 15:11:40 -0200

    no message

--- dist/js/ratchet.js
+++ dist/js/ratchet.js
@@ -1100,31 +1100,18 @@
 
 }(jQuery);
 
-(function( $ ){
+(function($){
+  var methods = {
+    init : function(options) {
+      console.warn(options);
+      $(this).tooltip({
+        title: options,
+        trigger: 'manual'
+      }).tooltip('show');
+    }
+  };
 
-    var methods = {
-        init : function(options) {
-          console.warn(options);
-          $(this).tooltip({
-            title: options,
-            trigger: 'manual'
-          }).tooltip('show');
-        },
-        show : function( ) {    },// IS
-        hide : function( ) {  },// GOOD
-        update : function( content ) {  }// !!!
-    };
-
-    $.fn.errorMessage = function(methodOrOptions) {
-        if ( methods[methodOrOptions] ) {
-            return methods[ methodOrOptions ].apply( this, Array.prototype.slice.call( arguments, 1 ));
-        } else if ( typeof methodOrOptions === 'object' || ! methodOrOptions ) {
-            // Default to "init"
-            return methods.init.apply( this, arguments );
-        } else {
-            $.error( 'Method ' +  methodOrOptions + ' does not exist on jQuery.tooltip' );
-        }    
-    };
-
-
+  $.fn.errorMessage = function(methodOrOptions) {
+    return methods.init.apply( this, arguments );
+  };
 })( jQuery );

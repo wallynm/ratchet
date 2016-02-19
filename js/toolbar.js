72bb0bb9013c2fb1e36ef1fdcd3a43d9c57cf25b
Wallysson Nunes <wally.nm@gmail.com>
2016-02-19 17:27:12 -0200

    Removed multiplier

--- js/toolbar.js
+++ js/toolbar.js
@@ -21,7 +21,7 @@
   if ($('.ni-product-buy').length !== 0){
     var $content = $('.content');
     var toolbarHeight = $('.ni-toolbar').height();
-    $('.ni-footer').css('padding-bottom', toolbarHeight * 2);
+    $('.ni-footer').css('padding-bottom', toolbarHeight);
 
     $content.scroll(function() {
       var scrolled = isScrolledIntoView('.ni-product-buy');
